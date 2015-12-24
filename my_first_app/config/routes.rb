5b0a8ea67efa1bda0720799ee2bfaf1a70da0f91
drwiner <drwiner@ncsu.edu>
2015-12-23 20:13:38 -0500

    forgot to add lots of stuff so here it is

--- my_first_app/config/routes.rb
+++ my_first_app/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'courses/index'
+
+  get 'welcome/index'
+
+  get 'greeter/hello'
+  get 'greeter/goodbye'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
